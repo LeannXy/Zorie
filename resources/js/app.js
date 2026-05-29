eb5a541dd35f75cdfeb906a2cc4d6f0718a2c883
leannxy <khalifahid18@gmail.com>
2026-05-29 08:33:47 +0700

    revisi icon

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,49 @@
+import ApexCharts from 'apexcharts'
 import Alpine from 'alpinejs'
+import { createIcons, icons } from 'lucide'
+
+import TomSelect from "tom-select";
+import "tom-select/dist/css/tom-select.css";
 
 window.Alpine = Alpine
+window.ApexCharts = ApexCharts
 
 Alpine.start()
+
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+        createIcons({ icons });
+
+        let categoryElement =
+        document.querySelector(
+            "#categories"
+        );
+
+        if(
+            categoryElement &&
+            !categoryElement.tomselect
+        ){
+
+            window.categorySelect =
+            new TomSelect(
+                categoryElement,
+                {
+
+                    plugins:[
+                        'remove_button'
+                    ],
+
+                    placeholder:
+                    'Search categories...',
+
+                    create:false
+
+                }
+            );
+
+        }
+
+    }
+);
